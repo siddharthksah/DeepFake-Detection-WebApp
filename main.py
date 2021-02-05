07cb5b0a8436bfcaa18a35a28dab91a41b34d31e
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2021-02-05 18:38:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ st.write("""
 ## DeepFake Image Detector
 """)
 
+count = 0
+
 uploaded_file = st.file_uploader("Choose the image", type=['jpg', 'png'])
 
 if uploaded_file is not None:
@@ -33,48 +35,58 @@ if uploaded_file is not None:
     else:
         st.write("Please upload .JPG OR .PNG format file only!")
 
+    count = count + 1
+
+
+
+
 if st.button('Process Images'):
 
-    if extension == 'jpg':
+    if count == 0 :
+        st.write("Please choose a file!")
 
-        st.write("Processing...")
+    else:
 
-        np_image = uploaded_image
-        np_image = np.array(np_image).astype('float32') / 255
-        np_image = transform.resize(np_image, (224, 224, 3))
-        np_image = np.expand_dims(np_image, axis=0)
+        if extension == 'jpg':
 
-        model = keras.models.load_model("model.h5")
-        probab = model.predict(np_image)[0][0]
-        st.write("The probability of this image being real is: ")
-        st.write(probab)
+            st.write("Processing...")
 
-        if probab < 0.001 :
-            st.write("which means this image is most likely fake, do not trust everything you see on the internet.")
-            st.image('fake_drake.png', caption="Drake doesn't approve", use_column_width=True)
+            np_image = uploaded_image
+            np_image = np.array(np_image).astype('float32') / 255
+            np_image = transform.resize(np_image, (224, 224, 3))
+            np_image = np.expand_dims(np_image, axis=0)
 
-        if probab > 0.9 :
-            st.write("which means this image is most likely real, still do not trust everything you see on the internet.")
-            st.image('real_drake.jpg', caption="Drake approves", use_column_width=True)
+            model = keras.models.load_model("model.h5")
+            probab = model.predict(np_image)[0][0]
+            st.write("The probability of this image being real is: ")
+            st.write(probab)
+
+            if probab < 0.001 :
+                st.write("which means this image is most likely fake, do not trust everything you see on the internet.")
+                st.image('fake_drake.png', caption="Drake doesn't approve", use_column_width=True)
+
+            if probab > 0.9 :
+                st.write("which means this image is most likely real, still do not trust everything you see on the internet.")
+                st.image('real_drake.jpg', caption="Drake approves", use_column_width=True)
 
 
-    elif extension == 'png':
-        st.write("Processing...")
+        elif extension == 'png':
+            st.write("Processing...")
 
-        np_image = uploaded_image
-        np_image = np.array(np_image).astype('float32') / 255
-        np_image = transform.resize(np_image, (224, 224, 3))
-        np_image = np.expand_dims(np_image, axis=0)
+            np_image = uploaded_image
+            np_image = np.array(np_image).astype('float32') / 255
+            np_image = transform.resize(np_image, (224, 224, 3))
+            np_image = np.expand_dims(np_image, axis=0)
 
-        model = keras.models.load_model("model.h5")
-        probab = model.predict(np_image)[0][0]
-        st.write("The probability of this image being real is: ")
-        st.write(probab)
+            model = keras.models.load_model("model.h5")
+            probab = model.predict(np_image)[0][0]
+            st.write("The probability of this image being real is: ")
+            st.write(probab)
 
-        if probab < 0.001:
-            st.write("which means this image is most likely fake, do not trust everything you see on the internet.")
-            st.image('fake_drake.png', caption="Drake doesn't approve", use_column_width=True)
+            if probab < 0.001:
+                st.write("which means this image is most likely fake, do not trust everything you see on the internet.")
+                st.image('fake_drake.png', caption="Drake doesn't approve", use_column_width=True)
 
-        if probab > 0.9 :
-            st.write("which means this image is most likely real, still do not trust everything you see on the internet.")
-            st.image('real_drake.jpg', caption="Drake approves", use_column_width=True)
+            if probab > 0.9 :
+                st.write("which means this image is most likely real, still do not trust everything you see on the internet.")
+                st.image('real_drake.jpg', caption="Drake approves", use_column_width=True)
