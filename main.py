3ba49c2efbc307bdedf61ddb432b3bc9eb11e277
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2021-02-05 18:41:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import streamlit as st
 from PIL import Image
 from skimage import transform
 
+#showing header image
 st.image('./header.jpg', use_column_width=True)
 
 
@@ -13,17 +14,19 @@ st.write("""
 ## DeepFake Image Detector
 """)
 
+#using this count as a failsafe if the user does not upload the file and still clicks the "process image" button
 count = 0
 
+#upload button for the input image
 uploaded_file = st.file_uploader("Choose the image", type=['jpg', 'png'])
 
 if uploaded_file is not None:
 
+    #getting the file extension of the uploaded file
     file_name = uploaded_file.name
     extension = file_name.split(".")[1]
 
     if extension == "png":
-
         uploaded_image = Image.open(uploaded_file)
         st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
 
@@ -42,6 +45,7 @@ if uploaded_file is not None:
 
 if st.button('Process Images'):
 
+    #checking if user uploaded any file
     if count == 0 :
         st.write("Please choose a file!")
 
@@ -51,16 +55,19 @@ if st.button('Process Images'):
 
             st.write("Processing...")
 
+            #preprocessing the image to use in the trained model
             np_image = uploaded_image
             np_image = np.array(np_image).astype('float32') / 255
             np_image = transform.resize(np_image, (224, 224, 3))
             np_image = np.expand_dims(np_image, axis=0)
 
+            #loading pre trained model called model.h5, you can find the code to export this on my GitHub https://github.com/siddharthksah
             model = keras.models.load_model("model.h5")
             probab = model.predict(np_image)[0][0]
             st.write("The probability of this image being real is: ")
             st.write(probab)
 
+                    
             if probab < 0.001 :
                 st.write("which means this image is most likely fake, do not trust everything you see on the internet.")
                 st.image('fake_drake.png', caption="Drake doesn't approve", use_column_width=True)
