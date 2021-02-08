cb121631c5d8b8f749ade737dc88830d9e4e0be3
Siddharth Kumar Sah <siddharth123sk@gmail.com>
2021-02-08 13:36:49 +0800

    image repetition bug removal

--- main.py
+++ main.py
@@ -32,17 +32,30 @@ count = 0
 
 #upload button for the input image
 uploaded_file = st.file_uploader("Choose the image, please make sure the image is in the correct orientation", type=['jpg', 'png', 'jpeg'])
+
+if uploaded_file != None:
+
+    count = count + 1
+
+#st.write(uploaded_file)
+#st.write(count)
+
+#st.write(uploaded_file)
+
 url = ""
 url = st.text_input("Or paste the image URL here", 'https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg')
 
 if url != "https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg":
     count = count + 1
-
-
+else:
+    count = count + 2
+#
+# st.write('final count')
+#st.write(count)
 
 if uploaded_file is not None:
 
-    if count == 0:
+    if count == 3:
 
         #getting the file extension of the uploaded file
         file_name = uploaded_file.name
@@ -65,11 +78,20 @@ if uploaded_file is not None:
         else:
             st.write("Please upload .JPG , .JPEG OR .PNG format file only!")
 
+    elif count == 1:
+        pass
+        response = requests.get(url)
+        uploaded_image = Image.open(BytesIO(response.content))
+        st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+
+    elif count == 2:
+        response = requests.get('https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg')
+        uploaded_image = Image.open(BytesIO(response.content))
+        st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+
     else:
         pass
-        # response = requests.get('https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg')
-        # uploaded_image = Image.open(BytesIO(response.content))
-        # st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+
 
 
 
@@ -77,7 +99,7 @@ if uploaded_file is not None:
 if st.button('Real or Fake?'):
 
     #checking if user uploaded any file
-    if count > 0 :
+    if count == 1 :
         #st.write("Please choose a file!")
         response = requests.get(url)
         uploaded_image = Image.open(BytesIO(response.content))
@@ -106,7 +128,37 @@ if st.button('Real or Fake?'):
                 "which means this image is most likely real, still do not trust everything you see on the internet.")
             st.image('real_drake.jpg', caption="Drake approves", use_column_width=True)
 
-    else:
+    elif count == 2 :
+        #st.write("Please choose a file!")
+        response = requests.get('https://media.nature.com/lw800/magazine-assets/d41586-020-01430-5/d41586-020-01430-5_17977552.jpg')
+        uploaded_image = Image.open(BytesIO(response.content))
+        st.image(uploaded_image, caption='Uploaded Image', use_column_width=True)
+
+        st.write("Processing...")
+
+        # preprocessing the image to use in the trained model
+        np_image = uploaded_image
+        np_image = np.array(np_image).astype('float32') / 255
+        np_image = transform.resize(np_image, (224, 224, 3))
+        np_image = np.expand_dims(np_image, axis=0)
+
+        # loading pre trained model called model.h5, you can find the code to export this on my GitHub https://github.com/siddharthksah
+        model = keras.models.load_model("model.h5")
+        probab = model.predict(np_image)[0][0]
+        st.write("The probability of this image being real is: ")
+        #probab = 0.9369558
+        st.write(probab)
+
+        if probab < 0.001:
+            st.write("which means this image is most likely fake, do not trust everything you see on the internet.")
+            st.image('fake_drake.png', caption="Drake doesn't approve", use_column_width=True)
+
+        if probab > 0.9:
+            st.write(
+                "which means this image is most likely real, still do not trust everything you see on the internet.")
+            st.image('real_drake.jpg', caption="Drake approves", use_column_width=True)
+
+    elif count == 3:
 
         if extension == 'jpg' or extension == 'JPG':
 
@@ -182,13 +234,16 @@ if st.button('Real or Fake?'):
                 st.write("which means this image is most likely real, still do not trust everything you see on the internet.")
                 st.image('real_drake.jpg', caption="Drake approves", use_column_width=True)
 
+    else:
+        st.write('Please refresh the page!')
+
 
 st.write('\n')
 st.write('\n')
 st.write('\n')
 
 st.write("""
-#### Made by Siddharth """)
+#### Made with :heart: by Siddharth """)
 # st.markdown(
 #     """<a href="https://www.siddharthsah.com/">siddharthsah.com</a>""", unsafe_allow_html=True,
 # )
